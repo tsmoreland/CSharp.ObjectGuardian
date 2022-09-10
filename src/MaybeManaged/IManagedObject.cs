850a3324df4097d11a3a06492688cfa85d35d1ba
Terry Moreland <terry.s.moreland@gmail.com>
2022-09-10 18:34:18 +0100

    csproj-8 merge maybe managed into managed, it's too simialr to warrant a separate implementation#8 - removed MaybeManagedObject - move OrElse, OrThrow or Map functions from IMaybe... to IManagedObject and add placeholder (throwing) implementations to ManagedObject

--- src/MaybeManaged/IManagedObject.cs
+++ src/MaybeManaged/IManagedObject.cs
@@ -55,5 +55,60 @@ public interface IManagedObject<T> : IDisposable
     /// </summary>
     bool HasValue { get; }
 
+    /// <summary>
+    /// Returns <see cref="Value"/> if <see cref="HasValue"/> is <see langword="true"/> or
+    /// <paramref name="other"/>
+    /// </summary>
+    /// <param name="other">alternative value used if <see cref="HasValue"/> is <see langword="false"/></param>
+    /// <returns></returns>
+    T OrElse(T other);
 
+    /// <summary>
+    /// Returns <see cref="Value"/> if <see cref="HasValue"/> is <see langword="true"/> or
+    /// value supplied by <paramref name="supplier"/>
+    /// </summary>
+    /// <param name="supplier">
+    /// used to create an instance of <typeparamref name="T"/> if <see cref="HasValue"/> is <see langword="false"/>
+    /// </param>
+    /// <returns></returns>
+    T OrElse(Func<T> supplier);
+
+    /// <summary>
+    /// Returns <see cref="Value"/> if <see cref="HasValue"/> is <see langword="true"/> or
+    /// throw <see cref="InvalidOperationException"/>
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">
+    /// if <see cref="HasValue"/> is <see langword="false"/>
+    /// </exception>
+    T OrThrow();
+
+    /// <summary>
+    /// Returns <see cref="Value"/> if <see cref="HasValue"/> is <see langword="true"/> or
+    /// throw exception supplied by <paramref name="supplier"/>
+    /// </summary>
+    /// <param name="supplier">
+    /// function used to build exception thrown if value is not present
+    /// </param>
+    /// <returns>
+    /// Contained <typeparamref name="T"/> if present
+    /// </returns>
+    T OrThrow(Func<Exception> supplier);
+
+    /// <summary>
+    /// If a <see cref="HasValue"/> is <see langword="true"/>, apply the
+    /// provided <see cref="ManagedObject{TMapped}"/>-bearing mapping function to it,
+    /// return that result, otherwise return
+    /// an empty <see cref="IManagedObject{TMapped}"/>.
+    /// </summary>
+    /// <typeparam name="TMapped"></typeparam>
+    /// <param name="mapper">
+    /// mapper used to map <typeparamref name="T"/> to <typeparamref name="TMapped"/>; mapped function
+    /// should not dispose the owned type as that remains the responsibility of this instance
+    /// </param>
+    /// <returns>
+    /// <see cref="IManagedObject{TMapped}"/> containing mapped result if present for this instance;
+    /// otherwise an empty <see cref="IMaybeManagedObject{TMapped}"/>
+    /// </returns>
+    IManagedObject<TMapped> Map<TMapped>(Func<T, TMapped> mapper) where TMapped : IDisposable;
 }
